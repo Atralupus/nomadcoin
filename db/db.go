6cd988eb49a6034f48de34ccbd7755185fbdfcec
Atralupus <atralupus@icloud.com>
2022-06-05 16:29:14 +0900

    transaction

--- db/db.go
+++ db/db.go
@@ -5,34 +5,29 @@ import (
 	"github.com/boltdb/bolt"
 )
 
-var db *bolt.DB
-
 const (
 	dbName       = "blockchain.db"
 	dataBucket   = "data"
 	blocksBucket = "blocks"
-	checkpoint   = "checkpoint"
+
+	checkpoint = "checkpoint"
 )
+
+var db *bolt.DB
 
 func DB() *bolt.DB {
 	if db == nil {
 		dbPointer, err := bolt.Open(dbName, 0600, nil)
-		utils.HandleErr(err)
 		db = dbPointer
-
-		err = db.Update(func(tx *bolt.Tx) error {
-			_, err := tx.CreateBucketIfNotExists([]byte(dataBucket))
+		utils.HandleErr(err)
+		err = db.Update(func(t *bolt.Tx) error {
+			_, err := t.CreateBucketIfNotExists([]byte(dataBucket))
 			utils.HandleErr(err)
-
-			_, err = tx.CreateBucketIfNotExists([]byte(blocksBucket))
-			utils.HandleErr(err)
-
+			_, err = t.CreateBucketIfNotExists([]byte(blocksBucket))
 			return err
 		})
-
 		utils.HandleErr(err)
 	}
-
 	return db
 }
 
@@ -41,29 +36,27 @@ func Close() {
 }
 
 func SaveBlock(hash string, data []byte) {
-	err := DB().Update(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket([]byte(blocksBucket))
+	err := DB().Update(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(blocksBucket))
 		err := bucket.Put([]byte(hash), data)
 		return err
 	})
-
 	utils.HandleErr(err)
 }
 
-func SaveBlockChain(data []byte) {
-	err := DB().Update(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket([]byte(dataBucket))
+func SaveCheckpoint(data []byte) {
+	err := DB().Update(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(dataBucket))
 		err := bucket.Put([]byte(checkpoint), data)
 		return err
 	})
-
 	utils.HandleErr(err)
 }
 
-func CheckPoint() []byte {
+func Checkpoint() []byte {
 	var data []byte
-	DB().View(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket([]byte(dataBucket))
+	DB().View(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(dataBucket))
 		data = bucket.Get([]byte(checkpoint))
 		return nil
 	})
@@ -72,8 +65,8 @@ func CheckPoint() []byte {
 
 func Block(hash string) []byte {
 	var data []byte
-	DB().View(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket([]byte(blocksBucket))
+	DB().View(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(blocksBucket))
 		data = bucket.Get([]byte(hash))
 		return nil
 	})
